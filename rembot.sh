fa923b97b2d161a1b904702230e81e3eb9834df5
EduardBaer <eduardbaer@users.noreply.github.com>
2017-07-12 16:15:28 +0200

    initial commit

---
+++ rembot.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# skip odd weeks, this script is executed on Sunday
+week=$(date +%V)
+if [ $(($week % 2)) != 0 ]
+then
+        exit
+fi
+
+treffcount=$(cat ~/rembot/treffcount)
+treffdate=$(LANG="de_DE.UTF-8" date --date="Wed" "+%-d. %B")
+
+from="From: RemBot <rembot@chaostreff-gun.de>"
+to="talk@chaostreff-gun.de"
+subject="${treffcount}. Treffen am ${treffdate}"
+
+mail -a "${from}" -s "${subject}" "${to}" <<EOF
+Hallo,
+
+diesen Mittwoch ist wieder Chaostreff!
+
+Wir treffen uns im Bastelkeller. Los geht's um 19 Uhr, vermutlich ist aber auch ab 18 Uhr schon jemand da.
+
+Dein Rem(inder)Bot
+EOF
+
+echo "$(($treffcount + 1))" > ~/rembot/treffcount
